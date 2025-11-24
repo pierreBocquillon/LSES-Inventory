7d0de4d7be1bcff134559a638fec7909ef971b63
BOCQUILLON Pierre <bocquillon.pierre@orange.fr>
2025-11-24 09:20:20 +0100

    remove old files

--- src/config/roles.js
+++
@@ -1,8 +0,0 @@
-let roles = [
-  { role: 'User', name: 'Médecin' },
-  { role: 'PoleStock', name: 'Pôle stock' },
-  { role: 'Direction', name: 'Direction' },
-  { role: 'Admin', name: 'Administrateur' }
-]
-
-export default roles
