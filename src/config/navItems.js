805089d4bad313bc8e80a4ec1f9cf0a5abceff1d
Rémy <bretonremy1@gmail.com>
2026-01-10 02:32:35 +0100

    [ADD] Link RH view to index

--- src/config/navItems.js
+++ src/config/navItems.js
@@ -1,25 +1,26 @@
 let navItems = [
   [
-    { title: 'Accueil', icon: 'mdi-home', link: '/'},
+    { title: 'Accueil', icon: 'mdi-home', link: '/' },
   ],
   [
-    { title: 'Inventaire', icon: 'mdi-archive', link: '/inventory'},
-    { title: 'Commandes', icon: 'mdi-file-document', link: '/orders'},
-    { title: 'Notes de frais', icon: 'mdi-receipt-text-check', link: '/expenseNotes'},
-    { title: 'Garage', icon: 'mdi-ambulance', link: '/garage'},
+    { title: 'Inventaire', icon: 'mdi-archive', link: '/inventory' },
+    { title: 'Commandes', icon: 'mdi-file-document', link: '/orders' },
+    { title: 'Notes de frais', icon: 'mdi-receipt-text-check', link: '/expenseNotes' },
+    { title: 'Garage', icon: 'mdi-ambulance', link: '/garage' },
   ],
   [
-    { title: 'Entreprises', icon: 'mdi-store', link: '/companies'},
-    { title: 'Stockages', icon: 'mdi-treasure-chest-outline', link: '/storage'},
-    { title: 'Items', icon: 'mdi-hamburger', link: '/items'},
-    { title: 'Vehicules', icon: 'mdi-helicopter', link: '/vehicles'},
+    { title: 'Entreprises', icon: 'mdi-store', link: '/companies' },
+    { title: 'Stockages', icon: 'mdi-treasure-chest-outline', link: '/storage' },
+    { title: 'Items', icon: 'mdi-hamburger', link: '/items' },
+    { title: 'Vehicules', icon: 'mdi-helicopter', link: '/vehicles' },
   ],
   // [
   //   { title: 'Autopsie', icon: 'mdi-coffin', link: '/autopsie'},
   // ],
   [
-    { title: 'Utilisateurs', icon: 'mdi-account-group', link: '/users'},
-    { title: 'Logs', icon: 'mdi-account-group', link: '/logs'},
+    { title: 'Utilisateurs', icon: 'mdi-account-group', link: '/users' },
+    { title: 'Logs', icon: 'mdi-account-group', link: '/logs' },
+    { title: 'Ressources Humaines', icon: 'mdi-account-tie', link: '/rh' },
   ],
 ]
 
